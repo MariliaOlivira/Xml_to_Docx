4095d8f965dc42ca898e048c94b269016e951c67
Marilia <marilia.araujo@academico.ifpb.edu.br>
2023-01-07 15:57:03 -0300

    fazendo translações entre as páginas

--- src/executavel/main.py
+++ src/executavel/main.py
@@ -20,7 +20,7 @@ class App(tk.Tk):
             self.frames[page_name] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame("StartPage")
+        self.show_frame("PageOne")
 
     def show_frame(self, page_name):
         frame = self.frames[page_name]
@@ -34,7 +34,9 @@ class StartPage(tk.Frame):
         
         # create a label
         label = tk.Label(self, text="Cadastro de Empresas",
-                         font=controller.title_font)
+                        font=controller.title_font)
+        # image = tk.PhotoImage(file="image/fundo.jpg")
+        # label = tk.Label(self, image=image)
         label.pack(side="top", fill="x", pady=10)
 
         # create a button
@@ -64,14 +66,15 @@ class StartPage(tk.Frame):
 
     def on_cadastro(self):
 
-        #criando uma aqrquivo txt
-        arquivo = open('arquivo.txt', 'w')
 
         #pegando os dados do usuario
         cnpj = self.cnpj_entry.get()
         endereco = self.endereco_entry.get()
         razao_social = self.razao_social_entry.get()
         inscricao_estadual = self.inscricao_estadual_entry.get()
+
+        #criando uma aqrquivo txt
+        arquivo = open(f'{cnpj}.txt', 'w')
 
         #escrevendo os dados no arquivo
         arquivo.write(f"CNPJ: {cnpj}")
@@ -100,15 +103,32 @@ class PageOne(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+
+
         label = tk.Label(self, text="This is page 1",
-                         font=controller.title_font)
+                        font=controller.title_font)
+        self.cnpj_label = tk.Label(self, text="CNPJ:")
+        self.cnpj_entry = tk.Entry(self)
+
+        self.cnpj_label.pack()
+        self.cnpj_entry.pack()
         label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
+        button = tk.Button(self, text="Veja existe",
+                            command=self.search_enterprise)
         button.pack()
 
-    def funcao_do_botao(self):
-        self.controller.show_frame("PageTwo")
+    def funcao_do_botao(self, page):
+        self.controller.show_frame(page)
+
+    def search_enterprise(self):
+        try:
+            cnpj = self.cnpj_entry.get()
+            open(f'{cnpj}.txt', 'r')
+            messagebox.showinfo(title="Atenção!", message="Sua empresa foi encontrada")
+            self.funcao_do_botao("PageTwo")
+        except:
+            messagebox.showinfo(title="Atenção!", message="Sua empresa não foi encontrada, efetue o cadastro")
+            self.funcao_do_botao("StartPage")
 
 
 class PageTwo(tk.Frame):
@@ -116,112 +136,13 @@ class PageTwo(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         label = tk.Label(self, text="This is page 2",
-                         font=controller.title_font)
+                        font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
+        button = tk.Button(self, text="Voltar para a página inicial",
+                            command=lambda: controller.show_frame("PageOne"))
         button.pack()
 
 
 app = App()
 app.mainloop()
 
-
-# # este é o front
-# # Biblioteca necessaria para a construcao da interface.
-# # from tkinter import *
-# from tkinter import *
-# from tkinter import ttk
-# from tkinter import filedialog as dlg
-# # from tkinter import *
-# from tkinter import messagebox
-
-
-# def compactar():
-#     arquivo_loc = dlg.askopenfilename(title="select a file", filetypes=(
-#         ("xml files", "*.xml"), ("All files", "*")))
-
-#     try:
-#         messagebox.showinfo(
-#             title="Oi", message="tudo bom?")
-#     except:
-#         messagebox.showerror(title="Error", message="Ops, algo deu errado!")
-#     else:
-#         messagebox.showinfo(
-#             title="Sucesso", message="Eita que tu é brabo msm!")
-
-# class App(Tk):
-#     def __init__(self):
-#         super().__init__()
-#         self.title("Páginas no Tkinter")
-#         self.geometry("800x600+400+300")  # Defina o tamanho e a posição da janela
-
-#         # Crie o widget Notebook
-#         self.notebook = ttk.Notebook(self)
-
-#         # Crie as páginas
-#         self.page1 = Frame(self.notebook)
-#         self.page2 = Frame(self.notebook)
-
-#         # Adicione widgets à primeira página
-#         Label(self.page1, text="Página 1").pack()
-#         Button(self.page1, text="Ir para a página 2", command=self.on_button_clicked).pack()
-
-#         # Adicione widgets à segunda página
-#         Label(self.page2, text="Página 2").pack()
-#         Button(self.page2, text="Ir para a página 1", command=self.on_button_clicked).pack()
-
-#         # Adicione as páginas ao Notebook
-#         self.notebook.add(self.page1, text="Página 1")
-#         self.notebook.add(self.page2, text="Página 2")
-
-#         # Adicione o Notebook à janela
-#         self.notebook.pack()
-
-#     def on_button_clicked(self):
-#         # Esconda a página atual
-#         current_page = self.notebook.select()
-#         current_page.pack_forget()
-#         # Exiba a próxima página
-#         next_page = self.notebook.next()
-#         next_page.pack()
-
-# app = App()
-# app.mainloop()
-
-
-# class App(Tk):
-#     def __init__(self):
-#         super().__init__()
-#         self.title("Xml-to-docx")
-#         self.geometry("800x600+400+300")
-#         # Crie o widget Notebook
-#         self.notebook = ttk.Notebook(self)
-
-#         # Criando páginas
-#         self.page1 = Frame(self.notebook)
-#         self.page2 = Frame(self.notebook)
-
-#         # Paginas adiconadas
-#         self.notebook.add(self.page1, text="Cadastre a primeira empresa")
-#         self.notebook.add(self.page2, text="Alguma coisa mais que eu n sei")
-
-#         # widgets às páginas
-#         Label(self.page1, text="Pagina Utilizada para o cadastro de empresas").pack()
-#         Button(self.page1, text="Escolha seu aquivo",
-#                command=compactar, bg="#EE6A50").pack()
-#         Button(self.page1, text="Ir para a página 2",
-#                command=self.on_button_clicked).pack()
-
-#         # o Notebook à janela
-#         self.notebook.pack()
-
-#     def on_button_clicked(self):
-#         # Esconda a primeira página
-#         self.page1.pack_forget()
-#         # Exiba a segunda página
-#         self.page2.pack()
-
-
-app = App()
-app.mainloop()
